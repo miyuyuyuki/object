a155c93f08b32de55c4e8a1b0cd1c609e73584a5
miyuyuyuki <konbukatsuo2boshiajinomoto@yahoo.co.jp>
2021-10-08 21:28:12 +0900

    10/8-2

---
+++ src/basic/student/Student.java
@@ -0,0 +1,48 @@
+package basic.student;
+
+class Student {
+    //フィールドの宣言
+    private String name;
+    private String gender;
+    private int grade;
+
+    public Student(String name,String gender,int grade){
+        this.name = name;
+        this.gender = gender;
+        this.grade = grade;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", gender='" + gender + '\'' +
+                ", grade=" + grade +
+                '}';
+    }
+}
+
